bdb7e676c0c903395a8b9da22ba17b8108c09e75
sthallysson <sthallysson5@gmail.com>
2023-10-04 00:20:43 -0300

    :tada: initial commit

---
+++ js/script.js
@@ -0,0 +1,118 @@
+const hangmanImg = document.querySelector(".hangman-box img");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text span");
+const keyboard = document.querySelector(".keyboard");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
+
+const keys = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuess = 6;
+
+const resetGame = () => {
+  correctLetters = [];
+  wrongGuessCount = 0;
+
+  hangmanImg.src = `images/hangman-${wrongGuessCount}.svg`;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuess}`;
+  buttons.forEach((btn) => (btn.disabled = false));
+
+  wordDisplay.innerHTML = currentWord
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+
+  gameModal.classList.remove("show");
+};
+
+const getRandomWords = () => {
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+
+  currentWord = word.toLowerCase();
+
+  document.querySelector(".hint-text span").innerText = hint;
+
+  resetGame();
+};
+
+const addButtonsToKeyboard = (key) => {
+  keyboard.innerHTML += `<button class="key">${key}</button>`;
+};
+
+keys.forEach(addButtonsToKeyboard);
+
+const gameOver = (isVictory) => {
+  setTimeout(() => {
+    const modalText = isVictory
+      ? `Você encontrou a palavra:`
+      : `A palavra certa era:`;
+    gameModal.querySelector("img").src = `images/${
+      isVictory ? "victory" : "lost"
+    }.gif`;
+    gameModal.querySelector("h4").innerText = `${
+      isVictory ? "Parabéns!" : "Game Over!"
+    }`;
+    gameModal.querySelector(
+      "p"
+    ).innerHTML = `${modalText} <strong>${currentWord}</strong>`;
+    gameModal.classList.add("show");
+  }, 300);
+};
+
+const initGame = (button, clickedLetter) => {
+  if (currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        correctLetters.push(letter);
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    });
+  } else {
+    wrongGuessCount++;
+    hangmanImg.src = `images/hangman-${wrongGuessCount}.svg`;
+  }
+  button.disabled = true;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuess}`;
+
+  if (wrongGuessCount === maxGuess) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
+};
+
+const buttons = document.querySelectorAll(".key");
+
+buttons.forEach((btn) =>
+  btn.addEventListener("click", (e) => initGame(e.target, btn.innerHTML))
+);
+
+getRandomWords();
+
+playAgainBtn.addEventListener("click", getRandomWords);
---
+++ js/wordList.js
@@ -0,0 +1,24 @@
+const wordList = [
+  {
+    word: "Computador",
+    hint: "Dispositivo eletrônico usado para processamento de dados.",
+  },
+  { word: "Girafa", hint: "Um animal de pescoço longo encontrado na África." },
+  { word: "Piano", hint: "Um instrumento musical de teclas." },
+  {
+    word: "Montanha",
+    hint: "Uma elevação natural da terra, geralmente com picos.",
+  },
+  { word: "Cachoeira", hint: "Uma queda d'água natural em um rio ou riacho." },
+  { word: "Amizade", hint: "Um relacionamento afetuoso entre amigos." },
+  {
+    word: "Astronomia",
+    hint: "A ciência que estuda os corpos celestes no universo.",
+  },
+  { word: "Jardim", hint: "Um espaço cultivado com plantas e flores." },
+  {
+    word: "Viagem",
+    hint: "Uma jornada ou deslocamento de um lugar para outro.",
+  },
+  { word: "Felicidade", hint: "Um estado de contentamento e alegria." },
+];
